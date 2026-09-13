a8f1d9191042526aa10a5f2f9cadac35883091d9
LauraOrtiz11 <ortizlaurava@gmail.com>
2026-09-13 14:53:33 -0500

    build: eliminar archivos no utilizados

--- api/v1/api.py
+++ api/v1/api.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter
-from api.v1.endpoints import items
 from api.v1.endpoints.user_routes import router as user_router
 from api.v1.endpoints.client_routes import router as client_router  
 from api.v1.endpoints.role_routes import router as role_router
@@ -7,7 +6,6 @@ from api.v1.endpoints.greenhouse_routes import router as greenhouse_router
 
 api_router = APIRouter()
 
-api_router.include_router(items.router, prefix="/items", tags=["items"])
 api_router.include_router(greenhouse_router)
 api_router.include_router(user_router)
 api_router.include_router(client_router)
